ddcc575316e870f9102d27eb5a9661db04fbdc8e
Julián Boxler <boxlerjulian@hotmail.com>
2025-11-10 13:35:11 -0300

    comentario api resend no tenemos dominio todavia

--- src/app/api/resend/route.ts
+++ src/app/api/resend/route.ts
@@ -1,25 +1,25 @@
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request) {
-  try {
-    const { to, subject, html } = await request.json();
+// export async function POST(request: Request) {
+//   try {
+//     const { to, subject, html } = await request.json();
 
-    const data = await resend.emails.send({
-      from: 'Bazar M&M <noreply@bazarmm.com>',
-      to,
-      subject,
-      html,
-    });
+//     const data = await resend.emails.send({
+//       from: 'Bazar M&M <noreply@bazarmm.com>',
+//       to,
+//       subject,
+//       html,
+//     });
 
-    return NextResponse.json({ success: true, data });
-  } catch (error) {
-    console.error('Error al enviar email:', error);
-    return NextResponse.json(
-      { error: 'Error al enviar el correo electrónico' },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ success: true, data });
+//   } catch (error) {
+//     console.error('Error al enviar email:', error);
+//     return NextResponse.json(
+//       { error: 'Error al enviar el correo electrónico' },
+//       { status: 500 }
+//     );
+//   }
+// }
